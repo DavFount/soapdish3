aa59cf0dbbc4933ecd18bcee68a21e1a83cda391
DavFount <47730893+DavFount@users.noreply.github.com>
2023-05-10 00:29:28 -0700

    Initial Commit

---
+++ resources/js/Layouts/DefaultLayout.vue
@@ -0,0 +1,18 @@
+<script setup>
+import NavBar from "@/Components/Nav/NavBar.vue";
+import NavBarItem from "@/Components/Nav/NavBarItem.vue";
+</script>
+
+<template>
+    <div
+        class="min-h-screen bg-center bg-gray-100 dark:bg-gray-900">
+        <NavBar>
+
+        </NavBar>
+        <slot/>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,22 @@
+<template>
+    <Head title="Welcome"/>
+    
+
+</template>
+
+<script>
+import {Head, Link} from '@inertiajs/vue3';
+import DefaultLayout from "@/Layouts/DefaultLayout.vue";
+
+export default {
+    layout: DefaultLayout,
+    components: { Head, Link },
+    props: {
+        canLogin: Boolean,
+        canRegister: Boolean,
+    }
+}
+</script>
+
+<style scoped>
+</style>
